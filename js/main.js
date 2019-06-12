4cdfafb34502101cb7763e87b5268204b1c9de96
Christopher Aidan Kim <christopheraidank@gmail.com>
2019-06-11 18:06:15 -0700

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '75px',
+  slidesToShow: 1,
+  arrows: false,
+  dots: true
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '75px',
+  //       slidesToShow: 1
+  //     }
+  //   },
+  //   {
+  //     breakpoint: 480,
+  //     settings: {
+  //       arrows: false,
+  //       centerMode: true,
+  //       centerPadding: '40px',
+  //       slidesToShow: 1
+  //     }
+  //   }
+  // ]
+});
