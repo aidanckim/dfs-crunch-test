0a09fa4a5cc1b6fd9cb5e0aabffe32ce05b27d34
Christopher Aidan Kim <christopheraidank@gmail.com>
2019-06-12 17:31:13 -0700

    Fix cards for all sports
    - added jQuery to hide and show each sport

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('.center').slick({
   centerMode: true,
-  centerPadding: '75px',
+  centerPadding: '70px',
   slidesToShow: 1,
   arrows: false,
   dots: true,
@@ -25,4 +25,37 @@ $('.center').slick({
   //     }
   //   }
   // ]
+});
+
+
+// SPORTS
+
+// const icon = document.querySelector('.one');
+//
+// box.addEventListener('click', function() {
+//   $('.card-one').hide();
+// });
+
+$('.nfl').hide();
+$('.nba').hide();
+
+$('.one').click(function(e) {
+  $('.nfl').show();
+  $('.mlb').hide();
+  $('.nba').hide();
+  e.preventDefault();
+});
+
+$('.two').click(function(e) {
+  $('.nba').show();
+  $('.mlb').hide();
+  $('.nfl').hide();
+  e.preventDefault();
+});
+
+$('.three').click(function(e) {
+  $('.mlb').show();
+  $('.nfl').hide();
+  $('.nba').hide();
+  e.preventDefault();
 });
