607733b74b242e176445c772c39beac65269266b
Christopher Aidan Kim <christopheraidank@gmail.com>
2019-08-13 18:41:20 -0700

    final

--- js/main.js
+++ js/main.js
@@ -68,71 +68,71 @@ $('.center').slick({
 //   $('.card-one').hide();
 // });
 
-$('.nfl').hide();
-$('.nba').hide();
+// $('.nfl').hide();
+// $('.nba').hide();
 
-$('.one').click(function(e) {
-  $('.nfl').show();
-  $('.mlb').hide();
-  $('.nba').hide();
-  e.preventDefault();
-});
+// $('.one').click(function(e) {
+//   $('.nfl').show();
+//   $('.mlb').hide();
+//   $('.nba').hide();
+//   e.preventDefault();
+// });
 
-$('.two').click(function(e) {
-  $('.nba').show();
-  $('.mlb').hide();
-  $('.nfl').hide();
-  e.preventDefault();
-});
-
-$('.three').click(function(e) {
-  $('.mlb').show();
-  $('.nfl').hide();
-  $('.nba').hide();
-  e.preventDefault();
-});
+// $('.two').click(function(e) {
+//   $('.nba').show();
+//   $('.mlb').hide();
+//   $('.nfl').hide();
+//   e.preventDefault();
+// });
+//
+// $('.three').click(function(e) {
+//   $('.mlb').show();
+//   $('.nfl').hide();
+//   $('.nba').hide();
+//   e.preventDefault();
+// });
 
 
 // SPORTS ICON HOVER
 
-$(function() {
-  $("#football").on({
-    mouseenter: function() {
-      $(this).attr('src', 'img/football-hover.png');
-    },
-    mouseleave: function(){
-      $(this).attr('src', 'img/football.png');
-    }
-  });
-});
-
-$(function() {
-  $("#basketball").on({
-    mouseenter: function() {
-      $(this).attr('src', 'img/basketball-hover.png');
-    },
-    mouseleave: function(){
-      $(this).attr('src', 'img/basketball.png');
-    }
-  });
-});
-
-$(function() {
-  $("#baseball").on({
-    mouseenter: function() {
-      $(this).attr('src', 'img/baseball-hover.png');
-    },
-    mouseleave: function(){
-      $(this).attr('src', 'img/baseball.png');
-    }
-  });
-});
-
-
-
-function changeImage() {
-  var image = document.getElementById('football');
-  if (image.src.match("img/football.png")) {
-    image.src = "img/football-hover.png";
-  }
-}
+// $(function() {
+//   $("#football").on({
+//     mouseenter: function() {
+//       $(this).attr('src', 'img/football-hover.png');
+//     },
+//     mouseleave: function(){
+//       $(this).attr('src', 'img/football.png');
+//     }
+//   });
+// });
+//
+// $(function() {
+//   $("#basketball").on({
+//     mouseenter: function() {
+//       $(this).attr('src', 'img/basketball-hover.png');
+//     },
+//     mouseleave: function(){
+//       $(this).attr('src', 'img/basketball.png');
+//     }
+//   });
+// });
+//
+// $(function() {
+//   $("#baseball").on({
+//     mouseenter: function() {
+//       $(this).attr('src', 'img/baseball-hover.png');
+//     },
+//     mouseleave: function(){
+//       $(this).attr('src', 'img/baseball.png');
+//     }
+//   });
+// });
+//
+//
+//
+// function changeImage() {
+//   var image = document.getElementById('football');
+//   if (image.src.match("img/football.png")) {
+//     image.src = "img/football-hover.png";
+//   }
+// }
